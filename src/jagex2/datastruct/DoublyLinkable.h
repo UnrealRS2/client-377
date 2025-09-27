43d2c7a856db87f391db13917ef1611700901cf3
Tyler Bochard <nullpops.git@gmail.com>
2025-09-27 09:39:58 -0400

    add missing ObfuscatedName comments

--- src/jagex2/datastruct/DoublyLinkable.h
+++ src/jagex2/datastruct/DoublyLinkable.h
@@ -4,9 +4,12 @@
 
 namespace jagex2::datastruct {
     struct DoublyLinkable : Linkable {
+        //ObfuscatedName("DPPNUUMQ.e")
         DoublyLinkable* next2{nullptr};
+        //ObfuscatedName("DPPNUUMQ.f")
         DoublyLinkable* prev2{nullptr};
 
+        //ObfuscatedName("DPPNUUMQ.b()V")
         void uncache() {
             if (prev2) {
                 prev2->next2 = next2;
--- src/jagex2/datastruct/Linkable.h
+++ src/jagex2/datastruct/Linkable.h
@@ -3,10 +3,14 @@
 #include <cstdint>
 namespace jagex2::datastruct {
     struct Linkable {
+        //ObfuscatedName("ZUOIJLRD.a")
         std::int64_t key{0};
+        //ObfuscatedName("ZUOIJLRD.b")
         Linkable* next{nullptr};
+        //ObfuscatedName("ZUOIJLRD.c")
         Linkable* prev{nullptr};
 
+        //ObfuscatedName("ZUOIJLRD.a()V")
         void unlink() {
             if (prev) {
                 prev->next = next;
